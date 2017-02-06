1e60cdf1b2dc1a9a15913cecbdb6be5afc32a522
shantanushirpure <shantanu.shirpure@live.com>
2017-02-05 22:52:54 -0500

    minor levftnav changes

---
+++ src/pages/frontend.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default class FrontEnd extends React.Component {
+  render() {
+    // const { title } = this.props;
+
+    return (
+      <div class="container-fluid">
+
+      
+
+    </div>
+    );
+  }
+}
